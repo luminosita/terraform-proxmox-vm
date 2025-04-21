cc798da6a1f80244b55604d00de290b41a36bc9e
Milos Milosavljevic <milosh@emisia.net>
2025-04-22 00:15:58 +0200

    mac_addresses

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "ipv4_addresses" {
     value = [ for k in proxmox_virtual_environment_vm.this : k.ipv4_addresses ]
 }
+
+output "mac_addresses" {
+    value = [ for k in proxmox_virtual_environment_vm.this : k.mac_addresses ]
+}
