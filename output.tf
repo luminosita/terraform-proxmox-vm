10c80c7bf1653037ff9e8ee5e33103b738b74adf
Milos Milosavljevic <milosh@emisia.net>
2025-04-22 00:27:11 +0200

    interface names

--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
 output "ipv4_addresses" {
-    value = { for k in proxmox_virtual_environment_vm.this : k.name => k.ipv4_addresses }
+  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.ipv4_addresses }
 }
 
 output "mac_addresses" {
-    value = { for k in proxmox_virtual_environment_vm.this : k.name => k.mac_addresses }
+  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.mac_addresses }
+}
+
+output "network_interface_names" {
+  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.network_interface_names }
 }
