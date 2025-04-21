6b63fe6ea39a62d2b53c2fa5e6581d4c2b0ed7e9
Milos Milosavljevic <milosh@emisia.net>
2025-04-22 01:03:38 +0200

    eth0

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
+locals {
+  indexes = { for k in proxmox_virtual_environment_vm.this : k.name => { for index, name in k.network_interface_names : k.network_interface_names[index] => index }}
+}
+
 output "ipv4_addresses" {
-  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.ipv4_addresses }
+  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.ipv4_addresses[local.indexes[k.name]["eth0"]] }
 }
 
 output "mac_addresses" {
-  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.mac_addresses }
-}
-
-output "network_interface_names" {
-  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.network_interface_names }
+  value = { for k in proxmox_virtual_environment_vm.this : k.name => k.mac_addresses[local.indexes[k.name]["eth0"]] }
 }
