ad4292b5078e7a0091e291aaeecffd410d319d35
Milos Milosavljevic <milosh@emisia.net>
2025-04-22 00:25:31 +0200

    tuple

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "ipv4_addresses" {
-    value = [ for k in proxmox_virtual_environment_vm.this : k.ipv4_addresses ]
+    value = { for k in proxmox_virtual_environment_vm.this : k.name => k.ipv4_addresses }
 }
 
 output "mac_addresses" {
-    value = { for k in proxmox_virtual_environment_vm.this : k.node_name => k.mac_addresses }
+    value = { for k in proxmox_virtual_environment_vm.this : k.name => k.mac_addresses }
 }
